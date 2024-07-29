ca8dc3600fac1c5bc81561ac98cd0f9042814ea9
pedram-pema <studentpedramp@gmail.com>
2024-07-29 14:09:39 +0330

    Reduce the size and order of script code
    Inline with clean colde, optimization, readabilty and also maintanence, modify the code in terms of length and layout.
    
    First, the two variables of gridUnits and divNum were moved from global scope to gridCreator function scope. This resulted in less code needed in gridChanger function and reset button event listener.
    Also align with this change, gridCreator function was optimized by eleminating gridUnits variable definition. Instead, gridUnits was added to gridCreator function as an argument with default value of 16.
    In result, divNum variable was also removed from arguments of gridCreator as it wasn't need as an argument anymore. Now that gridUnits is presented as argument, divNum can be created automatically inside gridCreator function according to gridUnits value.
    
    randomRgbStyle function was placed after gridCreator for better readibilty and clean code.
    
    Inside gridChanger function, the prompt value was directly moved into a Number() function. And lastly, the two conditions for null prompt and empty prompt were combined into a single line.

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-let gridUnits = 16;
-let divNum = Math.pow(gridUnits, 2);
-
-/* take gridUnits and divNum into gridCreator
-take flex 1 from styles to js */
-
-function randomRgbStyle() {
-    
-    let red = Math.floor(Math.random() * 256);
-    let green = Math.floor(Math.random() * 256);
-    let blue = Math.floor(Math.random() * 256);
-
-    return `rgb(${red}, ${green}, ${blue})`;
-}
-
 let container = document.querySelector(".container");
-function gridCreator(divNum) { /* Dynamic grid creator */
+function gridCreator(gridUnits = 16) { /* Dynamic grid creator */
+
+    let divNum = Math.pow(gridUnits, 2);
 
     let i = 0;
     do {
@@ -27,16 +14,26 @@ function gridCreator(divNum) { /* Dynamic grid creator */
     while (i < divNum);
 
 }
-gridCreator(divNum);
+gridCreator();
+
+function randomRgbStyle() {
+    
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+}
 
 let gridDivs = document.querySelectorAll(".new-div");
 // Wrap below inside a function:
 gridDivs.forEach(elem => {
+    
     let opacity = 0;
     function opaciter() {
         opacity += 0.1;
         return opacity.toString();
-    } /* why it doesn't work if these are outside */
+    }
 
     elem.addEventListener("mouseenter", () => {
         
@@ -47,31 +44,29 @@ gridDivs.forEach(elem => {
 
 let gridChangerBtn = document.querySelector(".grid-changer");
 function gridChanger() {
-    let sqrPrSide = prompt("How many squares per side do you want for the new grid?", "");
+    let sqrPrSide = Number(prompt("How many squares per side do you want for the new grid?", ""));
     if (sqrPrSide > 100) {
         alert("Grid can't have more than 100 squares per side, Enter smaller value");
         gridChanger();
-    } else if (sqrPrSide == null) {
         return;
-    } else if (sqrPrSide == "") {
+    } else if (sqrPrSide == null || sqrPrSide == "") {
         return;
     }
-    let numSqrPrSide = Number(sqrPrSide);
     container.innerHTML = "";
-    gridUnits = numSqrPrSide;
-    divNum = Math.pow(gridUnits, 2);
-    gridCreator(divNum); /* grid units */
+    gridCreator(sqrPrSide);
     let gridDivs = document.querySelectorAll(".new-div");
     gridDivs.forEach(elem => {
-    let opacity = 0;
-    function opaciter() {
-        opacity += 0.1;
-        return opacity.toString();
-    }
-    elem.addEventListener("mouseenter", () => {
-        elem.style.opacity = opaciter();
-        elem.style.backgroundColor = randomRgbStyle();
-    })
+        
+        let opacity = 0;
+        function opaciter() {
+            opacity += 0.1;
+            return opacity.toString();
+        }
+        
+        elem.addEventListener("mouseenter", () => {
+            elem.style.opacity = opaciter();
+            elem.style.backgroundColor = randomRgbStyle();
+        })
 })
 }
 
@@ -80,16 +75,15 @@ gridChangerBtn.addEventListener('click', gridChanger);
 let resetBtn = document.querySelector(".reset");
 resetBtn.addEventListener("click", () => {
     container.innerHTML = "";
-    gridUnits = 16;
-    divNum = Math.pow(gridUnits, 2);
-    gridCreator(divNum);
+    gridCreator();
     let gridDivs = document.querySelectorAll(".new-div");
     gridDivs.forEach(elem => {
+
         let opacity = 0;
         function opaciter() {
             opacity += 0.1;
             return opacity.toString();
-        } /* why it doesn't work if these are outside */
+        }
     
         elem.addEventListener("mouseenter", () => {
             
@@ -104,9 +98,8 @@ resetBtn.addEventListener("click", () => {
 // TODO: color changer enable/disable button
 // TODO: make mouse interact with squares by holding click
 // TODO: change color on the grid without erasing
-// TODO: fix flex-basis calculation
 // TODO: refactor
-// FIX: above 100 then 12
 // TODO: change mouse hold to toggle
 // TODO: Add eraser button
 // TODO: Add a color palette
+// TODO: Add (y) x (z) and total and amount of colored
